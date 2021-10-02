1154450e7e2b870ab026c14c3d3049b606d604d2
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2021-10-02 17:33:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <StoreProvider store={store}>
           <h2 style={{flex:1,textAlign: 'center', justifyContent: 'center',color: 'aqua', backgroundColor: 'black',margin:'20px',borderRadius:'15px'}}>YAPILACAKLAR LİSTESİ</h2>
           <h4 style={{flex:1,textAlign: 'center', justifyContent: 'center',borderRadius:'15px'}}>2 Ekim 2021</h4>
-          <h6 style={{flex:1,textAlign: 'center', justifyContent: 'center',borderRadius:'15px'}}>@Enes Battal-Enes Polat</h6>
+          <h6 style={{flex:1,textAlign: 'center', justifyContent: 'center',borderRadius:'15px'}}>@Enes Battal-Enes Polat-Tarık Karabey</h6>
         <AddTodo />
         <TodoList />
         
